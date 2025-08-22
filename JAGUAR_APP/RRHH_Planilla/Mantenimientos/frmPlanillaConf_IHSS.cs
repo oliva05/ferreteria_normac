d5d910439968a7765d13eaf3e1ec4f61c8846d75
RubenGarciia08 <andregarciia08@gmail.com>
2025-08-22 11:40:26 -0600

    planilla

---
+++ JAGUAR_APP/RRHH_Planilla/Mantenimientos/frmPlanillaConf_IHSS.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAGUAR_PRO.RRHH_Planilla.Mantenimientos
+{
+    public partial class frmPlanillaConf_IHSS : DevExpress.XtraEditors.XtraForm
+    {
+        public frmPlanillaConf_IHSS()
+        {
+            InitializeComponent();
+        }
+    }
+}
